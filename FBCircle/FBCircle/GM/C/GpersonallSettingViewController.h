c3931fe0fff354121703b5b75268f051792486df
gaomengaz <gaomengaz@163.com>
2014-05-13 16:25:22 +0800

    个人设置界面 6个button
    gaomeng commit1

---
+++ FBCircle/FBCircle/GM/C/GpersonallSettingViewController.h
@@ -0,0 +1,18 @@
+//
+//  GpersonallSettingViewController.h
+//  FBCircle
+//
+//  Created by gaomeng on 14-5-13.
+//  Copyright (c) 2014年 szk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GpersonallSettingViewController : UIViewController
+
+
+
+
+
+@end
---
+++ FBCircle/FBCircle/GM/C/GpersonallSettingViewController.m
@@ -0,0 +1,90 @@
+//
+//  GpersonallSettingViewController.m
+//  FBCircle
+//
+//  Created by gaomeng on 14-5-13.
+//  Copyright (c) 2014年 szk. All rights reserved.
+//
+
+#import "GpersonallSettingViewController.h"
+
+@interface GpersonallSettingViewController ()
+
+@end
+
+@implementation GpersonallSettingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    if( ([[[UIDevice currentDevice] systemVersion] doubleValue]>=7.0)) {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+    }
+    
+    
+    //头像
+    UIImageView *userFaceImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 107, 107)];
+    userFaceImageView.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:userFaceImageView];
+    
+    //用户名
+    UILabel *nameLable = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMaxX(userFaceImageView.frame)+23, 20, 100, 25)];
+    nameLable.backgroundColor = [UIColor purpleColor];
+    [self.view addSubview:nameLable];
+    
+    //个性签名
+    UILabel *qianmingLable = [[UILabel alloc]initWithFrame:CGRectMake(nameLable.frame.origin.x, CGRectGetMaxY(nameLable.frame)+10, 160, 30)];
+    qianmingLable.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:qianmingLable];
+    
+    
+    
+    
+    //按钮
+    NSArray *btnNameArray = @[@"我的相册",@"我的好友",@"我的消息",@"清除缓存",@"意见反馈",@"关于FB"];
+    for (int i=0; i<6; i++) {
+        NSString *str = btnNameArray[i];
+        UIButton *btn=[[UIButton alloc]initWithFrame:CGRectMake(110*(i%3), (330/3)*(i/3)+110, 107, 107)];
+        [btn setTitle:[NSString stringWithFormat:@"%@",str] forState:UIControlStateNormal];
+        btn.tag = i+10;
+        [btn addTarget:self action:@selector(btnClicked:) forControlEvents:UIControlEventTouchUpInside];
+        btn.backgroundColor=[UIColor grayColor];
+        [self.view addSubview:btn];
+    }
+    
+    
+}
+
+
+
+-(void)btnClicked:(UIButton*)sender{
+    NSLog(@"%ld",sender.tag);
+    //我的相册  10
+    //我的好友  11
+    //我的消息  12
+    //清除缓存  13
+    //意见反馈  14
+    //关于FB   15
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
